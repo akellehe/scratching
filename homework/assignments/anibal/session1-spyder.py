6edac17ac3ea7b77a83f6f1fde59ac6152edf277
MicroAnibal <developer.anibal.mendonca@gmail.com>
2020-11-19 12:32:15 +0000

    Add response callback support.
    Change assignment name to session1-spyder.py

--- homework/assignments/anibal/session1-spyder.py
+++ homework/assignments/anibal/session1-spyder.py
@@ -31,6 +31,7 @@ async def get(url: str = None, counter: int = 0, session: aiohttp.ClientSession 
     return await result.text()
 
 
+
 async def get_many_using_gather(urls: List[str], session: aiohttp.ClientSession = None) -> List[str]:
     """
     Get several pages CONCURRENTLY, given a list of URLs.
@@ -78,6 +79,58 @@ def print_elapsed_time(start_time: datetime.datetime, message: str = 'Duration: 
     print(f'{message} {str(elapsed_time)}')
 
 
+
+async def get_with_callback(url: str = None, counter: int = 0, session: aiohttp.ClientSession = None, _callback = None) -> str:
+    """
+    Get a webpage's content, given it's URL. Uses only a simple GET HTTP Request
+
+    :param str url: A fully qualified URL to fetch.
+    :param int counter: Which call is this? Order matters!
+    :param aiohttp.ClientSession session: A re-usable session for making calls.
+    :param callback(): function to be called when the information is received
+
+    :return: str The result.
+    """
+    if session is None:
+        raise ValueError('`session` is a required argument. You must pass an HTTP Session')
+    logger.info(f'Awaiting {url} in call {counter}.')
+    try:
+        result = await session.get(url)
+        logger.info(f'{url} was returned in the {counter} call.')
+
+        if _callback:
+            _callback(url, await result.text())
+
+        return await result.text()
+
+    except:
+        logger.info(f'{url} ERROR!')
+        return ''
+
+
+
+def get_many_using_tasks_with_callback(urls: List[str], session : aiohttp.ClientSession = None, response_callback = None) -> List[asyncio.Task]:
+    if session is None:
+        raise ValueError('`session` is a required argument. You must pass an HTTP Session')
+    tasks = []
+    for counter, url in enumerate(urls):
+        mycoroutine = get_with_callback(url, counter, session, response_callback)
+        tasks.append(asyncio.create_task(mycoroutine))
+    return tasks
+
+
+
+def example_callback(url: str, text: str):
+    """
+    Simple callback example function
+
+    :param str url: previous request url
+    :param str text: response.text
+    """
+    print (f'example_callback: \n url:  {url}\n text size(chars): {len(text)}')
+
+
+
 async def main():
     start_time = datetime.datetime.utcnow().replace(microsecond=0)
 
@@ -88,12 +141,12 @@ async def main():
         logger.info('Awaiting many pages using asyncio.Tasks')
         logger.info('-------------------------------------------')
         logger.info('-------------------------------------------')
-        tasks = get_many_using_tasks([
+        tasks = get_many_using_tasks_with_callback([
             'https://staging.titfortat.io',
             'https://staging.titfortat.io',
             'https://staging.titfortat.io',
             'https://staging.titfortat.io',
-        ], session=session)
+        ], session=session, response_callback=example_callback)
         done, pending = await asyncio.wait(tasks)
 
 
